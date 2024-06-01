759ea9c346a01bab6045b01f0d0bf010537227a0
Talha Sheikh <167472327+Sheikh-talha01@users.noreply.github.com>
2024-06-01 21:57:11 +0500

    Update binary operators overloading.cpp

--- binary operators overloading.cpp
+++ binary operators overloading.cpp
@@ -1,1 +1,43 @@
 
+#include<iostream>
+using namespace std;
+
+class Point
+{
+    int x,y;
+    public:
+    Point(int a=0, int b=0)
+	{
+		x = a;
+		y = b;
+	}
+    Point operator + (const Point obj)
+    {
+    	return(x + obj.x, y + obj.y);
+	}
+	int setter()
+	{
+		cout<<"Enter value for x: "<<endl;
+		cin>>x;
+		cout<<"Enter value for y: "<<endl;
+		cin>>y;
+	}
+	void show()
+	{
+		cout<<x<<endl<<y<<endl;
+	}
+    
+};
+
+int main()
+{
+    Point p,j;
+    cout<<"Enter values: "<<endl;
+    p.setter();
+    j.setter();
+	 Point result = p + j;
+	  
+    cout<<"Values are : "<<endl;
+    result.show();
+
+}
