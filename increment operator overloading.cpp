d9872df4ef848eabfbdee750a293c47a15b9d09a
Talha Sheikh <167472327+Sheikh-talha01@users.noreply.github.com>
2024-06-02 12:33:16 +0500

    Update increment operator overloading.cpp

--- increment operator overloading.cpp
+++ increment operator overloading.cpp
@@ -1,1 +1,39 @@
 
+#include<iostream>
+using namespace std;
+
+class Point
+{
+	int x,y;
+	public:
+	Point(int a=0, int b=0)
+	{
+		x = a;
+		y = b;	
+	}	
+	Point operator ++()
+	{
+		return (x++, y++);
+	}
+	void setter()
+	{
+		cout<<"Enter value for x: "<<endl;
+		cin>>x;
+		cout<<"Enter value for y: "<<endl;
+		cin>>y;
+	}
+	void getter()
+	{
+		cout<<"X is: "<<x<<endl;
+		cout<<"Y is: "<<y<<endl;
+	}
+};
+
+int main()
+{
+	Point obj1, obj2;
+	obj1.setter();
+	obj1.getter();
+	obj2 = ++obj1;
+	obj2.getter();
+}
